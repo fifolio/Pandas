0d3bdcbe1ac0472b762021fc3235a74d9745e99a
Firas <97522852+fifolio@users.noreply.github.com>
2026-08-22 15:35:50 +0000

    Pandas Getting Started

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello from pandas!")
+import pandas as pd
+
+mydataset = {
+  'cars': ["BMW", "Volvo", "Ford"],
+  'passings': [3, 7, 2]
+}
+
+myvar = pd.DataFrame(mydataset)
+
+# print(myvar)
+
+print(pd.__version__)
