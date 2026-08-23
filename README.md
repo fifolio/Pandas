842b9d489ddbf203345c1664f23dddd7ebf83b7c
Firas <97522852+fifolio@users.noreply.github.com>
2026-08-23 12:33:51 +0000

    Pandas - Fixing Wrong Data

--- README.md
+++ README.md
@@ -153,3 +153,21 @@ Removing Rows
 * The result from the converting in the example above gave us a NaT value, which can be handled as a NULL value, and we can remove the row by using the dropna() method.
 
 ---
+
+Fixing Wrong Data
+
+* "Wrong data" does not have to be "empty cells" or "wrong format", it can just be wrong, like if someone registered "199" instead of "1.99".
+* Sometimes you can spot wrong data by looking at the data set, because you have an expectation of what it should be.
+* If you take a look at our data set, you can see that in row 7, the duration is 450, but for all the other rows the duration is between 30 and 60.
+* It doesn't have to be wrong, but taking in consideration that this is the data set of someone's workout sessions, we conclude with the fact that this person did not work out in 450 minutes.
+
+Replacing Values
+* One way to fix wrong values is to replace them with something else.
+* In our example, it is most likely a typo, and the value should be "45" instead of "450", and we could just insert "45" in row 7.
+* For small data sets you might be able to replace the wrong data one by one, but not for big data sets.
+* To replace wrong data for larger data sets you can create some rules, e.g. set some boundaries for legal values, and replace a ny values that are outside of the boundaries.
+
+Removing Rows
+* Another way of handling wrong data is to remove the rows that contains wrong data.
+* This way you do not have to find out what to replace them with, and there is a good chance you do not need them to do your analyses.
+
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import pandas as pd
 
-mydataset = {
-  'cars': ["BMW", "Volvo", "Ford"],
-  'passings': [3, 7, 2]
-}
+mydataset = {"cars": ["BMW", "Volvo", "Ford"], "passings": [3, 7, 2]}
 
 myvar = pd.DataFrame(mydataset)
 
@@ -24,7 +21,7 @@ myvar = pd.Series(a)
 # Create your own labels:
 a = [1, 7, 2]
 
-myvar = pd.Series(a, index = ["x", "y", "z"])
+myvar = pd.Series(a, index=["x", "y", "z"])
 
 # print(myvar)
 
@@ -41,33 +38,27 @@ myvar = pd.Series(calories)
 # Create a Series using only data from "day1" and "day2":
 calories = {"day1": 420, "day2": 380, "day3": 390}
 
-myvar = pd.Series(calories, index = ["day1", "day2"])
+myvar = pd.Series(calories, index=["day1", "day2"])
 
 # print(myvar)
 
 # Create a DataFrame from two Series:
-data = {
-  "calories": [420, 380, 390],
-  "duration": [50, 40, 45]
-}
+data = {"calories": [420, 380, 390], "duration": [50, 40, 45]}
 
 myvar = pd.DataFrame(data)
 
 # print(myvar)
 
 # Create a simple Pandas DataFrame:
-data = {
-  "calories": [420, 380, 390],
-  "duration": [50, 40, 45]
-}
+data = {"calories": [420, 380, 390], "duration": [50, 40, 45]}
 
-#load data into a DataFrame object:
+# load data into a DataFrame object:
 df = pd.DataFrame(data)
 
-# print(df) 
+# print(df)
 
 # Return row 0:
-#refer to the row index:
+# refer to the row index:
 # print(df.loc[0]) # Note: This example returns a Pandas Series.
 
 # Return row 0 and 1:
@@ -75,149 +66,132 @@ df = pd.DataFrame(data)
 # print(df.loc[[0, 1]]) # When using [], the result is a Pandas DataFrame.
 
 # Add a list of names to give each row a name:
-data = {
-  "calories": [420, 380, 390],
-  "duration": [50, 40, 45]
-}
+data = {"calories": [420, 380, 390], "duration": [50, 40, 45]}
 
-df = pd.DataFrame(data, index = ["day1", "day2", "day3"])
+df = pd.DataFrame(data, index=["day1", "day2", "day3"])
 
 # print(df.loc["day1"])
 
 # Load the CSV into a DataFrame:
-df = pd.read_csv('data.csv')
+df = pd.read_csv("data.csv")
 
 # print(df.to_string()) # Tip: use to_string() to print the entire DataFrame.
 
 # Print the DataFrame without the to_string() method:
-df = pd.read_csv('data.csv')
+df = pd.read_csv("data.csv")
 
-# print(df) 
+# print(df)
 
 # Check the number of maximum returned rows:
-# print(pd.options.display.max_rows) 
+# print(pd.options.display.max_rows)
 
 # Increase the maximum number of rows to display the entire DataFrame:
 pd.options.display.max_rows = 9999
 
-df = pd.read_csv('data.csv')
+df = pd.read_csv("data.csv")
 
-# print(df) 
+# print(df)
 
 # Load the JSON file into a DataFrame:
-df = pd.read_json('data.json')
+df = pd.read_json("data.json")
 
-# print(df.to_string()) 
+# print(df.to_string())
 
 # Load a Python Dictionary into a DataFrame:
 data = {
-  "Duration":{
-    "0":60,
-    "1":60,
-    "2":60,
-    "3":45,
-    "4":45,
-    "5":60
-  },
-  "Pulse":{
-    "0":110,
-    "1":117,
-    "2":103,
-    "3":109,
-    "4":117,
-    "5":102
-  },
-  "Maxpulse":{
-    "0":130,
-    "1":145,
-    "2":135,
-    "3":175,
-    "4":148,
-    "5":127
-  },
-  "Calories":{
-    "0":409,
-    "1":479,
-    "2":340,
-    "3":282,
-    "4":406,
-    "5":300
-  }
+    "Duration": {"0": 60, "1": 60, "2": 60, "3": 45, "4": 45, "5": 60},
+    "Pulse": {"0": 110, "1": 117, "2": 103, "3": 109, "4": 117, "5": 102},
+    "Maxpulse": {"0": 130, "1": 145, "2": 135, "3": 175, "4": 148, "5": 127},
+    "Calories": {"0": 409, "1": 479, "2": 340, "3": 282, "4": 406, "5": 300},
 }
 
 df = pd.DataFrame(data)
 
-# print(df) 
+# print(df)
 
 # Get a quick overview by printing the first 10 rows of the DataFrame:
-df = pd.read_csv('data.csv')
+df = pd.read_csv("data.csv")
 
 # print(df.head(10))
 
 # Print the first 5 rows of the DataFrame:
-df = pd.read_csv('data.csv')
+df = pd.read_csv("data.csv")
 
 # print(df.head())
 
 # Print the last 5 rows of the DataFrame:
-# print(df.tail()) 
+# print(df.tail())
 
 # Print information about the data:
-# print(df.info()) 
+# print(df.info())
 
 # Return a new Data Frame with no empty cells:
-df = pd.read_csv('data.csv')
+df = pd.read_csv("data.csv")
 
 new_df = df.dropna()
 
 # print(new_df.to_string())
 
 # Remove all rows with NULL values:
-df = pd.read_csv('data.csv')
+df = pd.read_csv("data.csv")
 
-df.dropna(inplace = True)
+df.dropna(inplace=True)
 
 # print(df.to_string())
 
 # Replace NULL values with the number 130:
-df = pd.read_csv('data.csv')
+df = pd.read_csv("data.csv")
 
-df.fillna(130, inplace = True)
+df.fillna(130, inplace=True)
 # print(df)
 
 # Replace NULL values in the "Calories" columns with the number 130:
-df = pd.read_csv('data.csv')
+df = pd.read_csv("data.csv")
 
 df.fillna({"Calories": 130}, inplace=True)
 
 # Calculate the MEAN, and replace any empty values with it:
-df = pd.read_csv('data.csv')
+df = pd.read_csv("data.csv")
 
 x = df["Calories"].mean()
 
 df.fillna({"Calories": x}, inplace=True)
 
 # Calculate the MEDIAN, and replace any empty values with it:
-df = pd.read_csv('data.csv')
+df = pd.read_csv("data.csv")
 
 x = df["Calories"].median()
 
 df.fillna({"Calories": x}, inplace=True)
 
 # Calculate the MODE, and replace any empty values with it:
-df = pd.read_csv('data.csv')
+df = pd.read_csv("data.csv")
 
 x = df["Calories"].mode()[0]
 
 df.fillna({"Calories": x}, inplace=True)
 
 # Convert to date:
-df = pd.read_csv('data.csv')
+df = pd.read_csv("data.csv")
 
-df['Date'] = pd.to_datetime(df['Date'], format='mixed')
+df["Date"] = pd.to_datetime(df["Date"], format="mixed")
 
 print(df.to_string())
 
 # Remove rows with a NULL value in the "Date" column:
-df.dropna(subset=['Date'], inplace = True)
+df.dropna(subset=["Date"], inplace=True)
 
+# Set "Duration" = 45 in row 7:
+df.loc[7, "Duration"] = 45
+
+
+# Loop through all values in the "Duration" column. If the value is higher than 120, set it to 120:
+for x in df.index:
+    if df.loc[x, "Duration"] > 120:
+        df.loc[x, "Duration"] = 120
+
+
+# Delete rows where "Duration" is higher than 120:
+for x in df.index:
+    if df.loc[x, "Duration"] > 120:
+        df.drop(x, inplace=True)
